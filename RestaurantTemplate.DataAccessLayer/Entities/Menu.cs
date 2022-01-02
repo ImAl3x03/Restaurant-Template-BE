7c3d619b011971e79d7cb5cd432b61f201bfc424
Alex Di Maria <alessandro.dimaria3@outlook.it>
2022-01-02 10:57:22 +0100

    Added Menu model

---
+++ RestaurantTemplate.DataAccessLayer/Entities/Menu.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace RestaurantTemplate.DataAccessLayer.Entities
+{
+    public class Menu
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+        public string Category { get; set; }
+
+        public List<string> Ingrediants { get; set; }
+        public List<string> Allergens { get; set; }
+
+        public double Price { get; set; }
+    }
+}
